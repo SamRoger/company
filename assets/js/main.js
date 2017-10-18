73cf7740fa1878631e5a7f6d22097b7d281245bd
Sam Almeida <samRAlmeida@icloud.com>
2017-10-18 15:21:14 -0400

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,77 @@
+$("document").ready(function(){
+
+
+
+$(".firstProductButton").on("click", function(){
+	$(".firstProductContainer").scrollintoview({
+    	duration: 800,
+	});
+})
+$(".secondProductButton").on("click", function(){
+	$(".secondProductContainer").scrollintoview({
+		duration: 800,
+	});
+})
+$(".thirdProductButton").on("click", function(){
+	$(".thirdProductContainer").scrollintoview({
+		duration: 800,
+	});
+})
+$(".home").on("click", function(){
+	$(".hero").scrollintoview({
+		duration: 800,
+	});
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})//ready close
