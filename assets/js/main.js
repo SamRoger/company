9bf07d42c91986a9cb7e4c49a9d54de8f46b6c67
Sam Almeida <samRAlmeida@icloud.com>
2017-10-18 18:54:27 -0400

    template 80%

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,26 +2,51 @@ $("document").ready(function(){
 
 
 
+
+$(".home").on("click", function(){
+	$(".hero").scrollintoview({
+		duration: 800,
+	});
+	console.log(scrollTop)
+})
+//-------------------------------------------------
 $(".firstProductButton").on("click", function(){
 	$(".firstProductContainer").scrollintoview({
     	duration: 800,
 	});
 })
+//-------------------------------------------------
 $(".secondProductButton").on("click", function(){
 	$(".secondProductContainer").scrollintoview({
 		duration: 800,
 	});
 })
+//-------------------------------------------------
 $(".thirdProductButton").on("click", function(){
 	$(".thirdProductContainer").scrollintoview({
 		duration: 800,
 	});
 })
-$(".home").on("click", function(){
-	$(".hero").scrollintoview({
-		duration: 800,
-	});
-})
+//-------------------------------------------------
+$('.slick-slider-exp1').slick({
+	autoplay: true,
+	autoplaySpeed: 30000,
+	arrows: true,
+	dots: true,
+	mobileFirst: true,
+	prevArrow: '<span class="slick-prev">&lt;</span>',
+	nextArrow: '<span class="slick-next">&gt;</span>',
+	slidesToShow: 1,
+	slidesToScroll: 1
+});
+//-------------------------------------------------
+
+
+
+
+
+
+
 
 
 
