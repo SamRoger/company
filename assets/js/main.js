638e72347a23e370e68e5180d51d4d19f5134a6f
Sam Almeida <samRAlmeida@icloud.com>
2017-10-26 10:30:45 -0400

    scroll animation and fade

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,155 @@
 $("document").ready(function(){
-
-
-
-
-$(".home").on("click", function(){
-	$(".hero").scrollintoview({
-		duration: 800,
+//-------------------------------------------------
+//-------------------------------------------------
+//-------------------------------------------------
+$(function (){
+	var controller = new ScrollMagic.Controller({
+		globalSceneOptions: {
+			triggerHook: 'onLeave'
+		}
 	});
-	console.log(scrollTop)
+	// get all slides
+	var slides = document.querySelectorAll("section.panel");
+	// create scene for every slide
+	for (var i=0; i<slides.length; i++) {
+		new ScrollMagic.Scene({
+				triggerElement: slides[i]
+			})
+			.setPin(slides[i])
+			// .addIndicators() // add indicators (requires plugin)
+			.addTo(controller);
+	}
+});
+//-------------------------------------------------
+//-------------------------------------------------
+//-------------------------------------------------
+// $.keyframe.define([{
+//     name: 'fadeOut',
+//     from: {'color': '#fff'},
+//     to: {'color': 'teal'}
+// }]);
+
+// $("li").on("mouseenter", function(){
+// 	$("li").playKeyframe({
+// 			    name: 'navFadeTeal',
+// 			    duration: "1s"
+// 	});
+// })
+//-------------------------------------------------
+//-------------------------------------------------
+//-------------------------------------------------
+$(window).scroll(function(){
+var scrollTop2 = ($(this).scrollTop())
+    if (scrollTop2 > 460) {
+    	$(".intro").fadeOut(800)
+    	$("p").fadeOut(800)
+    }else if(scrollTop2 < 460){
+    	$(".intro").fadeIn(800)
+    	$("p").fadeIn(800)
+    }
+});
+//-------------------------------------------------
+$(window).scroll(function(){
+var scrollTop2 = ($(this).scrollTop())
+	if(scrollTop2 > 805 && scrollTop2 < 1116){
+
+		$(".firstProductsOnly").fadeIn(800)
+	}else{
+		$(".firstProductsOnly").fadeOut(800)
+	}
+})
+//-------------------------------------------------
+$(window).scroll(function(){
+var scrollTop2 = ($(this).scrollTop())
+	if(scrollTop2 > 1610 && scrollTop2 < 1919){
+		$(".secondProductsOnly").fadeIn(800)
+	}else{
+		$(".secondProductsOnly").fadeOut(800)
+	}
+})
+//-------------------------------------------------
+$(window).scroll(function(){
+var scrollTop2 = ($(this).scrollTop())
+	if(scrollTop2 > 2409 && scrollTop2 < 2677){
+		$(".thirdProductsOnly").fadeIn(800)
+	}else{
+		$(".thirdProductsOnly").fadeOut(800)
+	}
+})
+//-------------------------------------------------
+//-------------------------------------------------
+//-------------------------------------------------
+$(".home").on("click", function(){
+	$("html, body").animate({ 
+		scrollTop: "0px",
+		specialEasing: {
+	    width: "ease",
+	    height: "easeOutBounce"
+    }
+
+	},1000);
+	console.log("about Button Clicked")
 })
 //-------------------------------------------------
 $(".firstProductButton").on("click", function(){
-	$(".firstProductContainer").scrollintoview({
-    	duration: 800,
-	});
+	$("html, body").animate({ 
+		scrollTop: "930vh",
+		specialEasing: {
+	    width: "ease",
+	    height: "easeOutBounce"
+    }
+
+	},1000);
+	console.log("about Button Clicked")
 })
 //-------------------------------------------------
 $(".secondProductButton").on("click", function(){
-	$(".secondProductContainer").scrollintoview({
-		duration: 800,
-	});
+	$("html, body").animate({ 
+		scrollTop: "1731vh",
+		specialEasing: {
+	    width: "ease",
+	    height: "easeOutBounce"
+    }
+
+	},1000);
+	console.log("about Button Clicked")
 })
 //-------------------------------------------------
 $(".thirdProductButton").on("click", function(){
-	$(".thirdProductContainer").scrollintoview({
-		duration: 800,
-	});
+	$("html, body").animate({ 
+		scrollTop: "2534vh",
+		specialEasing: {
+	    width: "ease",
+	    height: "easeOutBounce"
+    }
+
+	},1000);
+	console.log("about Button Clicked")
 })
 //-------------------------------------------------
-$('.slick-slider-exp1').slick({
-	autoplay: true,
-	autoplaySpeed: 30000,
-	arrows: true,
-	dots: true,
-	mobileFirst: true,
-	prevArrow: '<span class="slick-prev">&lt;</span>',
-	nextArrow: '<span class="slick-next">&gt;</span>',
-	slidesToShow: 1,
-	slidesToScroll: 1
+//-------------------------------------------------
+//-------------------------------------------------
+$(".testImage").on("click", function(){
+	$(".bodyContainer").fadeTo(300, 0.4)
+	$(".imageModal").fadeIn(300)
+})
+//-------------------------------------------------
+$(".close").on("click", function(){
+	$(".bodyContainer").fadeTo(300, 1)
+	$(".imageModal").fadeOut(300)
+})
+//-------------------------------------------------
+//-------------------------------------------------
+//-------------------------------------------------
+$(function() {
+    $(window).scroll(function() {
+       var tpos =  $(document).scrollTop();
+       $(".display").html(tpos)
+    });
 });
 //-------------------------------------------------
-
-
-
-
+//-------------------------------------------------
+//-------------------------------------------------
 
 
 
